88711d34de1e017342567e09b5cf5fe84482cadc
Vishnu-vardhan-220521 <166403826+Vishnu-vardhan-220521@users.noreply.github.com>
2025-02-28 12:07:57 +0530

    Delete gym directory

--- gym/src/com/gym/IUser.java
+++
@@ -1,6 +0,0 @@
-package com.gym;
-
-public interface IUser {
-	boolean register(User user,int index);
-	boolean login(String username,String password);
-}
--- gym/src/com/gym/IWorkout.java
+++
@@ -1,6 +0,0 @@
-package com.gym;
-
-public interface IWorkout {
-	void addWorkOut(Workout workout,int index);
-	Workout[] displayWorkOuts();
-}
